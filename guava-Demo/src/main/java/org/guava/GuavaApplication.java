6aed8dac3e344edef798e0150f7c3a0f05e4093a
LGY <1165370209@qq.com>
2024-01-23 17:01:26 +0800

    添加Mysql、mybatis

--- guava-Demo/src/main/java/org/guava/GuavaApplication.java
+++ guava-Demo/src/main/java/org/guava/GuavaApplication.java
@@ -1,5 +1,6 @@
 package org.guava;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @Version: 1.0
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@MapperScan("org.guava.CacheService.mapper")
 public class GuavaApplication
 {
     public static void main(String[] args) {
